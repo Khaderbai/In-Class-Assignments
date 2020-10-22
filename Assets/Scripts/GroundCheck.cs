68a2f3a64236ee94582cdd6609fb9673ed0ccc73
Khaderbai <el.tobstaz9@gmail.com>
2020-10-22 19:01:28 -0400

    Initial commit

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLayerMask;
+
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        isGrounded = collider != null && (((1 << collider.gameObject.layer) & platformLayerMask) != 0);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+}
